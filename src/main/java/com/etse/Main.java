332e9b814d7f13d5e5e0dc7cd8e3421a416dab71
Ryan <ryan.hunter@techelevator.com>
2024-03-26 17:38:39 -0500

    Main class

--- src/main/java/com/etse/Main.java
+++ src/main/java/com/etse/Main.java
@@ -6,6 +6,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        TransactionManager tm =  new TransactionManager();
+        Scanner input = new Scanner(System.in);
 
+        boolean running = true;
+
+        System.out.println("***********************************************************************************************************************");
+        System.out.println("***********************************************************************************************************************");
+        System.out.println("**************************                                                                   **************************");
+        System.out.println("**************************                    PERSONAL FINANCE TRACKER                       **************************");
+        System.out.println("**************************                                                                   **************************");
+        System.out.println("***********************************************************************************************************************");
+        System.out.println("***********************************************************************************************************************");
+
+
+        while (running){
+            System.out.println();
+            System.out.println("Choose an option: (1) Add Transaction (2) View Summary (3) Exit");
+            int choice = input.nextInt();
+            input.nextLine(); //consume next line
+
+            switch (choice){
+                case 1:
+
+                    System.out.println("Add a transaction type: ");
+                    String type = input.nextLine();
+                    if(!type.equalsIgnoreCase("Income") && !type.equalsIgnoreCase("Expense")){
+                        throw new RuntimeException("Invalid input, please enter Income or Expense");
+                    }
+
+                    System.out.println("Add an amount for your transaction: ");
+                    BigDecimal amount = input.nextBigDecimal();
+                    input.nextLine(); //consume next line
+
+                    System.out.println("Enter date (YYYY-MM-DD): ");
+                    LocalDate date = LocalDate.parse(input.nextLine());
+
+                    tm.addTransactions(type, amount, date);
+                    break;
+                case 2:
+                    tm.printSummary();
+                    break;
+                case 3:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option, please try again!");
+            }
+
+        }
+        input.close();
     }
 }
