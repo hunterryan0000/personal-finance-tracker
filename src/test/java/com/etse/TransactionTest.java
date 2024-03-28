b3606bf171ec5588521b062d93e071d6b22a5d22
Ryan <ryan.hunter@techelevator.com>
2024-03-27 20:01:41 -0500

    TransactionTest

--- src/test/java/com/etse/TransactionTest.java
+++ src/test/java/com/etse/TransactionTest.java
@@ -2,19 +2,50 @@ package com.etse;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
+    Transaction incomeTransaction = new Transaction("Income", new BigDecimal(100), LocalDate.of(2024, 3, 27));
+    Transaction expenseTransaction = new Transaction("Expense", new BigDecimal(50), LocalDate.parse("2024-03-27"));
+
+
 
     @Test
     void getType() {
+        String type = "Income";
+        assertNotNull(type);
+        assertNotNull(incomeTransaction.getType());
+        assertEquals(type, incomeTransaction.getType());
+        type = "Expense";
+        assertNotNull(type);
+        assertNotNull(expenseTransaction.getType());
+        assertEquals(type, expenseTransaction.getType());
+
     }
 
     @Test
     void getAmount() {
+        BigDecimal amount = new BigDecimal(100);
+        assertNotNull(amount);
+        assertNotNull(incomeTransaction.getAmount());
+        assertEquals(amount, incomeTransaction.getAmount());
+        amount = new BigDecimal(50);
+        assertNotNull(amount);
+        assertNotNull(expenseTransaction.getAmount());
     }
 
     @Test
     void getDate() {
+        LocalDate date = LocalDate.parse("2024-03-27");
+        assertNotNull(date);
+        assertNotNull(incomeTransaction.getDate());
+        assertEquals(date, incomeTransaction.getDate());
+        date = LocalDate.of(2024, 3, 27);
+        assertNotNull(date);
+        assertNotNull(expenseTransaction.getDate());
+        assertEquals(date, expenseTransaction.getDate());
     }
 }
